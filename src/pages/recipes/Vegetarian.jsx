c7da3003b9e9357bf38f297ce04404d7b2a40768
Stiven <stivenmartinez1510@gmail.com>
2024-09-19 10:30:50 -0500

    agragaron hover,after

--- src/pages/recipes/Vegetarian.jsx
+++ src/pages/recipes/Vegetarian.jsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom"
+import { recipesVegetarian } from "../../data/dataRecipes"
+import './Vegetarian.css'
+
 const Vegetarian = () => {
+  console.log(recipesVegetarian)
   return (
-    <div>Vegetarian</div>
+    <div>
+      {
+        recipesVegetarian.map((recipeVegetarian) => (
+          <div key={recipeVegetarian.id} className="card">
+            <img src={recipeVegetarian.img} alt={recipeVegetarian.nombre} />
+            <h3>Nombre: {recipeVegetarian.nombre} </h3>
+            <p>Descripcion: {recipeVegetarian.descipcion} </p>
+            <p>Dificultad: {recipeVegetarian.dificultad} </p>
+            <p>Tiempo: {recipeVegetarian.tiempo} </p>
+            <section className="botones">
+              <Link className="boton">Detalle</Link>
+              <Link className="boton">Editar</Link>
+              <Link className="boton">Eliminar</Link>
+             
+            </section>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
