0c9af2ca3c12b821606a201c47cae5c8039366f0
Stiven <stivenmartinez1510@gmail.com>
2024-10-17 08:56:37 -0500

    c

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,15 +1,68 @@
+import { useState, useEffect } from 'react'
 import './Register.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+let urlUsuarios = 'http://localhost:3000/users'
 
 const Register = () => {
+
+
+  const [stateName, setStateName] = useState('')
+  const [stateUser, setStateUser] = useState('')
+  const [statePassword, setStatePassword] = useState('')
+  const [stateEmail, setStateEmail] = useState('')
+  const [statePhone, setStatePhone] = useState('')
+  const [stateUsters, setStateUsers] = useState([])
+
+  function getUsers() {
+    fetch(urlUsuarios)
+      .then(response => response.json())
+      .then(json => setStateUsers(json))
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
+  function buscarUsuario() {
+    let auth = stateUsters.some((item) => stateUser == item.user && stateEmail == item.email)
+    console.log(auth)
+    return auth
+  }
+
+
+  function createUser() {
+    let newUser = {
+      user: stateUser,
+      email: stateEmail,
+      name: stateName,
+      password: statePassword,
+      phone: statePhone
+    }
+    fetch(urlUsuarios, {
+      method: 'POST',
+      body: JSON.stringify(newUser)
+    })
+    .then()
+    .then()
+
+
+  }
+
+  function registerUser() {
+
+  }
+
+
+
   return (
     <form className='registerForm'>
-        <input placeholder="User" type="text" />
-        <input placeholder="Password" type="text" />
-        <input placeholder="Email" type="text" />
-        <input placeholder="Phone" type="text" />
-        <input type="button" value="Register" />
-        <Link to='/login'>¿Ya tiene una cuenta?</Link>
+      <input onChange={(e) => { setStateName(e.target.value) }} placeholder="Name" type="text" />
+      <input onChange={(e) => { setStateUser(e.target.value) }} placeholder="User" type="text" />
+      <input onChange={(e) => { setStatePassword(e.target.value) }} placeholder="Password" type="text" />
+      <input onChange={(e) => { setStateEmail(e.target.value) }} placeholder="Email" type="text" />
+      <input onChange={(e) => { setStatePhone(e.target.value) }} placeholder="Phone" type="text" />
+      <input onClick={registerUser} type="button" value="Register" />
+      <Link to='/login'>¿Ya tiene una cuenta?</Link>
     </form>
   )
 }
